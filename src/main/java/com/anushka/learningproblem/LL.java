dbdfb092b1f6803f118ec5e71cbc1349e8e6af18
Anushka <anushkamar1102@gmail.com>
2023-12-01 12:49:37 +0530

    Created: linked list class and declared the variables

--- src/main/java/com/anushka/learningproblem/LL.java
+++ src/main/java/com/anushka/learningproblem/LL.java
@@ -1,4 +1,17 @@
 package com.anushka.learningproblem;
 
 public class LL {
+    private Node head;
+    private int size;
+    public LL(){
+        this.size = 0;
+    }
+    private class Node{
+        private int data;
+        private Node next;
+
+        public Node(int data){
+            this.data = data;
+        }
+    }
 }
