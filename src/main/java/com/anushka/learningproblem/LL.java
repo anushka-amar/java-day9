db9f22438080e6be1b7d5389849e15d2d0ee9918
Anushka <anushkamar1102@gmail.com>
2023-12-01 13:09:53 +0530

    Refactored: functionality to add a node before a node

--- src/main/java/com/anushka/learningproblem/LL.java
+++ src/main/java/com/anushka/learningproblem/LL.java
@@ -6,6 +6,31 @@ public class LL {
     public LL(){
         this.size = 0;
     }
+
+    public void addFirst(int data){
+        Node node = new Node(data);
+        if(head == null){
+            head = node;
+            node.next = null;
+            return;
+        }
+        node.next = head;
+        head = node;
+        size++;
+    }
+
+    public void displayLL(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+" -> ");
+            temp = temp.next;
+        }
+        System.out.print("NULL");
+    }
     private class Node{
         private int data;
         private Node next;
--- src/main/java/com/anushka/learningproblem/Main.java
+++ src/main/java/com/anushka/learningproblem/Main.java
@@ -1,4 +1,11 @@
 package com.anushka.learningproblem;
 
 public class Main {
+    public static void main(String[] args) {
+     LL list1 = new LL();
+     list1.addFirst(70);
+     list1.addFirst(30);
+     list1.addFirst(56);
+     list1.displayLL();
+    }
 }
